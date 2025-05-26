ed1175af738114c58b01cf1f2b3f86d529edb1d0
rizalyoga <ach.rizal.yogaswara@gmail.com>
2025-05-26 20:47:48 +0700

    fix: fix swagger not showing

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ app.use(
   swaggerUi.serve,
   swaggerUi.setup(swaggerSpec, {
     customCssUrl:
-      "https://l.facebook.com/l.php?u=https%3A%2F%2Fcdnjs.cloudflare.com%2Fajax%2Flibs%2Fswagger-ui%2F4.1.0%2Fswagger-ui.min.css%3Ffbclid%3DIwZXh0bgNhZW0CMTAAYnJpZBExeVdXYnFRemFrZ0Nhc3dKMAEerdmDq95qXZgzdXlJUWintKxhjrShAyMad-9DO6QtWszF_2lPAP5CxsUB-Oc_aem_p1V5gKxiQznas9vMyZ9oXA&h=AT3Ox666ez_z76EJtvzyfA56w5nxNYX49vPfUk_d6QQIw_DbzagKLvLnxcLL1gj6v-gWwx6S_yuvaue4b9zSkZlrHS8cqhFpuUFj3ctOWFKhEQuo3hAhI5lIgdQyg9OhjRo9LKN-&__tn__=R]-R&c[0]=AT14vT6bF9VSBmaTU_FHbyauA161PU2Pezo8bQuSTtbx27OTFIXB-NGoz3Pwz34phhTKqwEXV2lniAJ2ZnHf_vpxAEdWpPRkL9GdlR_aO03sMgnrCDzD-D0s_Yp1kqYtqS6J8gKVZXadklJSPohNHllybiqgiGZazwqkZA5W3yCMRe81Nolf0sc",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css?fbclid=IwY2xjawKhT_dleHRuA2FlbQIxMABicmlkETF5V1dicVF6YWtnQ2Fzd0owAR7gDe7qDiJdeTkJldofxnMLyyifakyhyeC2wX6fLkVm11tyRj1hGvvNgkWRhA_aem_TpLZQhS290xLTzEAgk5S6A",
   })
 );
 
